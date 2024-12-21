3cb3cb7354b744bad7457c564223f5db0c183e75
Middle <rna050906@gmail.com>
2024-12-21 18:46:56 +0300

    In this version, a mechanism was added to help adapt this application to different versions of Panda Mouse Pro

--- app/src/main/java/toolkit/patcher/MainHook.java
+++ app/src/main/java/toolkit/patcher/MainHook.java
@@ -6,6 +6,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.XposedHelpers;
+import java.util.Set;
 
 public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     public static final String TAG = "MyPatch";
@@ -13,16 +14,17 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("com.panda.mouse")){
-
-            Class<?> clazz = XposedHelper.findClass("com.chaozhuo.gameassistant.LicenseCheckerActivity$OooO0OO", lpparam.classLoader);
-            XposedHelper.findAndHookMethod(clazz, "dontAllow", int.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("Blocking dontAllow method call with argument: " + param.args[0]);
-		        XposedHelpers.callMethod(param.thisObject, "allow", param.args[0]);
-                param.setResult(null);
-                }
-            });
+            Set<Class<?>> classes = XposedHelper.findClassesByPrefix("com.chaozhuo.gameassistant.LicenseCheckerActivity$", lpparam.classLoader);
+            for (Class<?> clazz : classes) {
+                XposedHelper.findAndHookMethod(clazz, "dontAllow", int.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(TAG + " Blocking dontAllow method call with argument: " + param.args[0]);
+                        XposedHelpers.callMethod(param.thisObject, "allow", param.args[0]);
+                        param.setResult(null);
+                    }
+                });
+            }
         }
     }
 
